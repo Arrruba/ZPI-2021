a08c3a1166d9c73493a5675b7e674f86f38dae35
Agata Rudzka <56095972+Arrruba@users.noreply.github.com>
2021-11-09 15:20:29 +0100

    initiatives-add/list/vote

--- initiative_details.js
+++ initiative_details.js
@@ -1,9 +1,25 @@
+
 window.onload= function onload(){
     var url_string = window.location.href
     var url = new URL(url_string);
     var initiative_id = url.searchParams.get("id");
     var url_get_initiative = "http://localhost:9003/initiatives/"+initiative_id;
     console.log(url_get_initiative);
+
+    var arr = window.sessionStorage.getItem("InitiativesToVoteOn");
+    if(arr!=null){
+    var array = arr.split(",");
+
+    if(!array.includes(initiative_id)){
+        document.getElementById('vote').style.display = 'block';
+        document.getElementById('unvote').style.display = 'none';
+    }
+    else{
+        document.getElementById('vote').style.display = 'none';
+        document.getElementById('unvote').style.display = 'block';
+    }
+    }
+
 
     $.ajax(
         {
@@ -13,18 +29,81 @@ window.onload= function onload(){
             success: function(results){
 
                 var init_title=results.title;
-                var district=results.district;
+                var district=results.district.name;
                 var cost=results.estimatedCost;
                 var category=results.category.name;
                 var description=results.description;
+                var author=results.user.login;
                             
                 console.log(init_title);//+" "+author+" "+district);
                 $("#initiative-title").html((init_title));
                 $("#district-name").append((district));
                 $("#cost").append((cost)+" zł");
                 $("#category").append((category));
-                $('#description').append(description);
+                $('#details').append(description);
+                $('#author').append(author);
+
             }
         }
     );
+
+
+    
+}
+
+function voteOnInitiative(){
+    var url_string = window.location.href
+    var url = new URL(url_string);
+    var initiative_id = url.searchParams.get("id");
+    
+    var arr = window.sessionStorage.getItem("InitiativesToVoteOn");
+    var array ;
+    if(arr!=null){
+        array = arr.split(",");
+    }
+    
+    if (arr==null){
+        arr = initiative_id;
+        window.sessionStorage.setItem("InitiativesToVoteOn", arr);
+    }
+    else if(!array.includes(initiative_id) && array.length>0){
+        arr += ","+initiative_id;
+        window.sessionStorage.setItem("InitiativesToVoteOn", arr);
+    }
+    else if(!array.includes(initiative_id) && array.length==0){
+        arr = initiative_id;
+        window.sessionStorage.setItem("InitiativesToVoteOn", arr);
+    }
+
+    document.getElementById('vote').style.display = 'none';
+    document.getElementById('unvote').style.display = 'block';
+
+    // for (var i=1; i<arr.length(); i++)
+    //     counter++;
+
+    // init_array.push(initiative_id);
+    // window.sessionStorage.setItem("InitiativesToVoteOn", JSON.stringify(init_array));
+}
+
+function removeVote(){
+    var url_string = window.location.href
+    var url = new URL(url_string);
+    var initiative_id = url.searchParams.get("id");
+
+    var arr = window.sessionStorage.getItem("InitiativesToVoteOn");
+    var array = arr.split(",");
+    var arr_new;
+    array.forEach(element => {
+        if(element.localeCompare(initiative_id)!=0 && array.indexOf(element)>0){
+            arr_new += ","+element;
+    }
+        else if(element.localeCompare(initiative_id)!=0 && array.indexOf(element)==0){
+            arr_new = element;
+        }
+    });
+    
+    window.sessionStorage.setItem("InitiativesToVoteOn", arr_new);
+
+    document.getElementById('vote').style.display = 'block';
+    document.getElementById('unvote').style.display = 'none';
 }
--- initiatives.js
+++ initiatives.js
@@ -4,6 +4,17 @@ var count=0;
 window.onload= function onload(){
     var url_get_initiatives = "http://localhost:9003/initiatives/getAll";
     var id=1;
+
+    var arr = window.sessionStorage.getItem("InitiativesToVoteOn");
+    console.log("arr: "+arr);
+    var array=[];
+    if(arr!=null && arr.localeCompare("undefined")!=0){
+        array = arr.split(",");
+        console.log("eee");
+    }
+    console.log(array);
+
+
     $.ajax(
         {
             url: url_get_initiatives,
@@ -19,19 +30,26 @@ window.onload= function onload(){
                     if((item.description).length>=100)
                         descript +="...";
 
-                    htmlc += '<div id="element'+item.id+'" class="my-list-element" style="margin-top:2%;" onclick="location.href=\'initiative_details.html?id='+item.id+'\';">';
+                        htmlc += '<div id="filterable"><p style="display:none;">'+item.district.name+"</p>";
+
+                    if(array.length>0 && array.includes((item.id).toString())){
+                        htmlc += '<div id="element-'+item.id+'" class="my-chosen-list-element w-100" style="margin-top:2%;" onclick="location.href=\'initiative_details.html?id='+item.id+'\';">';
+                    }
+                    else{
+                        htmlc += '<div id="element-'+item.id+'" class="my-list-element w-100" style="margin-top:2%;" onclick="location.href=\'initiative_details.html?id='+item.id+'\';">';
+                    }
                     console.log('"location.href=\'initiative_details.html?id='+item.id+';"');
                     htmlc += '<table class="table my-tab-color w-100">';
                     htmlc += '<tbody id="tab-body">';
                          htmlc += '<tr><tr>';
                             htmlc += '<td style="font-size:0.25em; display:none;">'+item.id+'</td>';
                              htmlc += '<td id="row'+id+'-title" style="font-weight:bold; font-size:1.25em;">'+item.title+'</td>';
-                             htmlc += '<td id="row'+id+'-districtName" class="city-name-list" >'+item.district.name+'</td>';
+                             htmlc += '<td id="row'+id+'-districtName" class="city-name-list '+item.district.name+'" >'+item.district.name+'</td>';
                          htmlc += '</tr><tr>';
                              htmlc += '<td id="row'+id+'-description">'+descript+'</td>';
                         htmlc += '</tr>';
                     htmlc += '</tr>';
-                    htmlc += '<tr></tr></tbody></table></div>';
+                    htmlc += '<tr></tr></tbody></table></div></div>';
                     console.log(item.title+","+item.description);
                     count++;
                     id++;
@@ -42,6 +60,20 @@ window.onload= function onload(){
             }
         }
     );
+
+//     var arr = window.sessionStorage.getItem("InitiativesToVoteOn");
+//     if(arr!=null){
+//     var array = arr.split(",");
+//     if(array.length>0){
+//         array.forEach(element => {
+//             console.log("el"+element);
+//             var el_id = 'element-'+element;
+//             console.log("el_id "+el_id);
+
+//             document.getElementById(el_id).style.backgroundColor = '#5dca3e93';
+//         });
+//     }
+// }
 }
 
 
@@ -61,7 +93,7 @@ function showFilter(){
                 
                 $.each(results.districtList, function(key,item){
                   //  htmlc += '<li><a class="dropdown-item" href="#">'+ item.name +'</a></li>';
-                    htmlc += '<option value="1">'+ item.name +'</option>';
+                    htmlc += '<option value="'+ item.name +'" >'+ item.name +'</option>';
 
                 });
                 $("#dropdown-menu-districts").html((htmlc));
@@ -71,6 +103,28 @@ function showFilter(){
     document.getElementById('filter_field').style.display = 'block';
     }
     else     document.getElementById('filter_field').style.display = 'none';
+
+    $(document).ready(function(){
+        $("#dropdown-menu-districts").change(function() {
+            console.log("pick");
+          var value = $(this).val().toLowerCase();
+          console.log(value);
+          if(value.localeCompare("dzielnica")!=0){
+          $("#filterable div").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+                console.log($(this).text().toLowerCase());
+          });}
+          else{
+            $("#filterable div").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value)!= 0)
+          }); 
+          }
+        });
+    
+      });
+    // document.getElementById('filter-field').addEventListener('select', (e) => {
+    //     instance.input-group(e.target.value);
+    //   });
 
 }
 
